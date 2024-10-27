2a44083a5374e7e088a2bb81f9357bd43129f1c8
Soburr <adebesinbenzenejr99@gmail.com>
2024-10-27 16:21:00 +0100

    feature/checkout

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -27,6 +28,26 @@ class OrderController extends Controller
     }
 
     public function store(Request $request) {
-    
+
+        $request->validate([
+            'first_name' => 'required',
+            'last' => 'required',
+            'email' => 'required|email',
+            'street_address' => 'required',
+            'state' => 'required',
+            'city' => 'required',
+            'zip' => 'required',
+        ]);
+
+        Order::create([
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'email' => $request->email,
+            'street_address' => $request->street_address,
+            'state' => $request->state,
+            'city' => $request->city,
+            'zip' => $request->zip,
+        ]);
+
     }
 }
